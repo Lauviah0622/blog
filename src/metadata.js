0b94e583363715e926556e3c6c253d30f8749646
Lavi <lauviah0622@gmail.com>
2022-08-13 13:17:58 +0800

    refactor: header page

--- src/metadata.js
+++ src/metadata.js
@@ -3,10 +3,18 @@ export default {
   pageSize: {
     posts: 10,
     archive: 20,
-    tips: 12
-  
+    tips: 12,
   },
   path: {
     posts: '/src/posts/*.md',
   },
-};
+
+  navLink: {
+    landing: '/',
+    archive: '/archive',
+    tips: '/tips',
+    tags: '/tags',
+    collection: '/collection',
+    // about: '/about',
+  },
+}
