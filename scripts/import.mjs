fbcce140c4cdac5e61fdf0f4b09c2f3250a4f75f
Lavi <lauviah0622@gmail.com>
2022-09-17 12:22:43 +0800

    feat: collection import script

--- scripts/import.mjs
+++ scripts/import.mjs
@@ -6,14 +6,17 @@ import yaml from 'js-yaml'
 import * as path from 'path'
 import gradient from 'gradient-string'
 import Table from 'cli-table3'
+import dayjs from 'dayjs'
 // const fs = require('fs')
 // const { program } = require('commander')
 // const fm = require('front-matter')
 
 const POST_LAYOUT = '/src/layouts/Post.astro'
+const COLLECTION_LAYOUT = '/src/layouts/Collection.astro'
 const IMAGE_FOLDER = './public/assets/images/post/'
 const POST_FOLDER = './src/pages/post'
 const TIP_FOLDER = './src/pages/tip'
+const COLLECTION_FILE = './src/pages/collection.md'
 const HEAD = [
   'filename',
   'willImport',
@@ -31,6 +34,7 @@ const HEAD = [
 const IMAGE = ['no image', 'pass', 'missing']
 const POSTS_SOURCE = process.env.POSTS_SOURCE
 const TIPS_SOURCE = process.env.TIPS_SOURCE
+const COLLECTION_SOURCE = process.env.COLLECTION_SOURCE
 // const POST_FOLDER = './test/dist/'
 
 const printSafe = (text) => console.log(gradient.mind(text))
@@ -46,13 +50,13 @@ program.command('posts').action(() => {
 
   printSafe('prev: ', files)
 
-  check(markdowns)
+  check(markdowns, postCheck)
 
   printSafe(`copy image start`)
   copyImage(markdowns)
   printSafe(`copy image done`)
   printSafe(`copy post start`)
-  copyPost(markdowns, POST_FOLDER)
+  clonePost(markdowns, POST_LAYOUT, POST_FOLDER)
   printSafe(`copy post done`)
 })
 
@@ -62,24 +66,43 @@ program.command('tips').action(() => {
   printSafe(`create Md model...`)
   const markdowns = markdownsConstructor(files)
 
-  check(markdowns)
+  check(markdowns, postCheck)
 
   printSafe(`copy image start`)
   copyImage(markdowns)
   printSafe(`copy image done`)
   printSafe(`copy post start`)
-  copyPost(markdowns, TIP_FOLDER)
+  clonePost(markdowns, POST_LAYOUT, TIP_FOLDER)
   printSafe(`copy post done`)
 })
 
-program.command('collect <source>').action((source) => {
-  console.log(source)
+program.command('collection').action(() => {
+  printSafe(`loading tips from ${COLLECTION_SOURCE}...`)
+  const file = loadPath(COLLECTION_SOURCE)
+  const markdowns = markdownsConstructor([file])
+  check(markdowns, collectionCheck)
+
+  printSafe(`copy image start`)
+  copyImage(markdowns)
+  printSafe(`copy image done`)
+  printSafe(`copy post start`)
+  const [markdown] = markdowns
+  console.log(markdown)
+  clonePost(markdown, COLLECTION_LAYOUT, COLLECTION_FILE)
+  printSafe(`copy post done`)
+  // console.log({ markdown })
 })
 
 program.parse()
 
 function printTable(data) {
-  const head = HEAD
+  const columns = data.reduce((columns, datum) => {
+    Object.keys(datum).forEach((key) => {
+      columns.add(key)
+    })
+    return columns
+  }, new Set())
+  const head = [...columns]
   const table = new Table({
     head,
     // colWidths: Arra
@@ -106,9 +129,9 @@ function printTable(data) {
   console.log(table.toString())
 }
 
-function check(markdowns) {
+function check(markdowns, checkFunc) {
   const data = markdowns.reduce((prev, md) => {
-    const check = markdownCheck(md)
+    const check = checkFunc(md)
     prev.push(check)
 
     md.willImport = check.willImport
@@ -118,32 +141,57 @@ function check(markdowns) {
   printTable(data)
 }
 
-function copyPost(markdowns, distDir) {
-  markdowns
-    .filter(({ willImport }) => willImport)
-    .map((mk) => {
-      mk.frontmatter.attributes['layout'] = POST_LAYOUT
-      return mk
-    })
-    .forEach((mk) => {
-      if (!mk.willImport) return
-      const frontMatterText = yaml.dump(mk.frontmatter.attributes, {
-        styles: {
-          '!!timestamp': 'YYYY-MM-DD',
-        },
+function clonePost(markdown, layout, distDir) {
+  if (Array.isArray(markdown)) {
+    // multi files
+    markdown
+      .filter(({ willImport }) => willImport)
+      .map((mk) => {
+        // TODO 這個東西要抽出來，放到 patch 階段去處理
+        mk.frontmatter.attributes['layout'] = layout
+        return mk
       })
+      .forEach((mk) => {
+        if (!mk.willImport) return
+        const frontMatterText = yaml.dump(mk.frontmatter.attributes, {
+          styles: {
+            '!!timestamp': 'YYYY-MM-DD',
+          },
+        })
 
-      const body = mk.frontmatter.body
+        const body = mk.frontmatter.body
 
-      const post = `---
+        const post = `---
 ${frontMatterText}
 ---
 ${body}`
 
-      const fileName = path.basename(mk.path)
-      const dist = path.normalize(`${distDir}/${fileName}`)
-      fs.writeFileSync(dist, post, { encoding: 'utf-8' })
+        const fileName = path.basename(mk.path)
+        const dist = path.normalize(`${distDir}/${fileName}`)
+        fs.writeFileSync(dist, post, { encoding: 'utf-8' })
+      })
+  } else {
+    // currently onlt collection use
+    const mk = { ...markdown }
+    if (!markdown.willImport) return
+    // TODO 這個東西要抽出來，放到 patch 階段去處理
+    mk.frontmatter.attributes['layout'] = layout
+    mk.frontmatter.attributes['updateData'] = dayjs().format('YYYY.MM.DD')
+    const frontMatter = yaml.dump(mk.frontmatter.attributes, {
+      styles: {
+        '!!timestamp': 'YYYY-MM-DD',
+      },
     })
+    const body = mk.frontmatter.body
+    const post = `---
+${frontMatter}
+---
+${body}`
+    const dist = distDir
+
+    console.log('123123', { dist, post })
+    fs.writeFileSync(dist, post, { encoding: 'utf-8' })
+  }
 }
 
 function copyImage(markdowns) {
@@ -227,20 +275,49 @@ function loadPath(loadedPath) {
     return files
   } else if (stats.isFile()) {
     const content = fs.readFileSync(loadedPath, 'utf-8')
-    const filename = path.basename(filepath)
-    return [
-      {
-        path: filepath,
-        content,
-        filename,
-      },
-    ]
+    const filename = path.basename(loadedPath)
+    return {
+      path: loadedPath,
+      content,
+      filename,
+    }
   }
 
   return []
 }
 
-function markdownCheck(md) {
+function collectionCheck(md) {
+  const frontmatter = md.frontmatter
+
+  const hasFrontmatter = !!frontmatter?.frontmatter
+  const pubDate = !!frontmatter.attributes?.pubDate
+  const willImport = hasFrontmatter && !!pubDate
+
+  const imagesExist = md.images
+    .filter(({ source }) => isLocalPath(source))
+    .map(({ source }) => {
+      const mkAbsPath = path.resolve(md.path)
+      const mkAbsDir = path.dirname(mkAbsPath)
+      const imagePath = path.resolve(`${mkAbsDir}/${source}`)
+
+      return { exist: fs.existsSync(imagePath), source }
+    })
+
+  const isImagesExist = imagesExist.every(({ exist }) => exist)
+  const image = md.images.length === 0 ? 0 : isImagesExist ? 1 : 2
+
+  const check = {
+    filename: md.filename,
+    frontmatter: hasFrontmatter,
+    pubDate,
+    layout: !!frontmatter.attributes?.layout,
+    willImport,
+    image,
+  }
+  return check
+}
+
+function postCheck(md) {
   const frontmatter = md.frontmatter
 
   const title = !!frontmatter.attributes?.title
